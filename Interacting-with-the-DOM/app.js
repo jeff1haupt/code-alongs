61a362e43f9587bf87c15a7ba3ceb128c6681913
jeff1haupt <jhaupt@southbendin.gov>
2022-01-20 22:23:52 -0500

    updated for 10-21-fesd class

--- Interacting-with-the-DOM/app.js
+++ Interacting-with-the-DOM/app.js
@@ -1,18 +1,40 @@
 let button = document.getElementById('btn');
 let content = document.getElementById('content');
 
-button.addEventListener('click', () => {
-    if (content.innerHTML == 'Goodbye.') {
-        content.innerHTML = 'Hello.';
-    } else {
-        content.innerHTML = 'Goodbye.';
-    }
+// document.getElementById('paragraphs').innerHTML = `<h2>I changed this <em>element</em></h2>`;
+// let div = document.getElementById("paragraphs");
+// document.getElementById("content").style.backgroundColor = "red";
+// document.getElementById("content").style.color = "white";
+
+// let newParagraph = document.createElement("p");
+// (console.log(newParagraph));
+// newParagraph.innerText = "some text is now here";
+// (console.log(newParagraph));
+// div.appendChild(newParagraph);
+
+// let elements = document.getElementsByClassName("button");
+// for( let i = 0; i < elements.length; i++) {
+
+// }
+// console.log(document.getElementsByTagName("p"));
+// console.log(document.querySelector("div").firstChild);
+// console.log(document.querySelectorAll("#content"));
+console.log(button);
+
+function showAlert() {
+    alert("the button was clicked");
+}
+button.addEventListener('click', (event) => {
+    let sibling = event.target.nextSibling.nextSibling.nextSibling;
+    let body = document.getElementsByTagName("body")[0];
+    body.removeChild(sibling);
+    showAlert();
 });
 
 document.getElementById('remove').addEventListener('click', () => {
     let idToRemove = document.getElementById('remove-id').value;
     let elementToRemove = document.getElementById(idToRemove);
-    elementToRemove.parentNode.removeChild(content);
+    elementToRemove.parentNode.removeChild(idToRemove);
     document.getElementById('remove-id').value = '';
 });
 
